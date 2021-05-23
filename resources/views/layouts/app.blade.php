12e6c9f53cfc2382538a24f66f780f65485a4d7b
Fede Serron <f.serron.umpierrez@gmail.com>
2021-05-23 13:24:42 -0300

    Navigation menu optimized, Login & logout routes assigned

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,21 +17,14 @@
 
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
+        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     </head>
     <body class="font-sans antialiased">
         <x-jet-banner />
 
         <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
+            @livewire('navigation')
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
 
             <!-- Page Content -->
             <main>
