33e47557af73ef36a7ce3cace8246432d8d36e79
Fede Serron <f.serron.umpierrez@gmail.com>
2021-05-30 13:43:30 -0300

    PostSeeder

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -19,10 +19,10 @@ class PostSeeder extends Seeder
         $posts = Post::factory(100)->create();
 
         foreach($posts as $post){
-            // Image::factory(1)->create([
-            //     'imageable_id' => $post->id,
-            //     'imageable_type' => Post::class
-            // ]);
+            Image::factory(1)->create([
+                'imageable_id' => $post->id,
+                'imageable_type' => Post::class
+            ]);
             $post->tags()->attach([
                 rand(1, 4),
                 rand(5, 8)
